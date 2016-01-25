9257ee6a062d8965497202da3ad5a3b7f3af9599
Brian Donovan <me@brian-donovan.com>
2016-01-25 09:04:35 -0800

    Move source files into `src/`.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'index.js',
+  entry: 'src/index.js',
   plugins: [babel({
     babelrc: false,
     presets: ['es2015-rollup'],
